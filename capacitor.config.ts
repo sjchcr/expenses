64e0ba1b28ed06301db6f91d732bca7aaa6637c6
Steven Jimenez Chaves <sjch.cr@gmail.com>
2026-02-02 20:36:04 -0600

    Add Capacitor iOS support

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'cr.steven.expenses',
+  appName: 'Financial Tracker',
+  webDir: 'dist'
+};
+
+export default config;
